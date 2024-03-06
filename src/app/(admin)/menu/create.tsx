33f2d74bceb24cafa15ae7a150ed98af38e77ae7
aniketjha9179 <aniketjha917928@gmail.com>
2024-03-06 17:10:35 +0530

    Added image picker for profile icon

--- src/app/(admin)/menu/create.tsx
+++ src/app/(admin)/menu/create.tsx
@@ -3,13 +3,14 @@ import React, { useState } from "react";
 import Button from "@/src/components/Button";
 import Colors from "@/src/constants/Colors";
 import * as ImagePicker from 'expo-image-picker';
+import { Stack } from "expo-router";
 
 
 const CreateProductScreen = () => {
   const [name, setName] = useState("");
   const [price, setPrice] = useState("");
   const [errors, setErrors] = useState("");
-   const [image, setImage] = useState(null);
+   const [image, setImage] = useState<string |null> (null);
 
   const resetFields = () => {
     setName("");
@@ -44,7 +45,8 @@ const CreateProductScreen = () => {
   const pickImage = async () => {
     // No permissions request is necessary for launching the image library
     let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.All,
+                                            // we can set any media type
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
       allowsEditing: true,
       aspect: [4, 3],
       quality: 1,
@@ -60,7 +62,8 @@ const CreateProductScreen = () => {
 
   return (
     <View style={styles.container}>
-      <Image  source={{uri:'https://t3.ftcdn.net/jpg/00/27/57/96/360_F_27579652_tM7V4fZBBw8RLmZo0Bi8WhtO2EosTRFD.jpg'}} style={styles.img} />
+      <Stack.Screen options={{title:'Create Product'  }} />
+      <Image  source={{uri:image || 'https://t3.ftcdn.net/jpg/00/27/57/96/360_F_27579652_tM7V4fZBBw8RLmZo0Bi8WhtO2EosTRFD.jpg'}} style={styles.img} />
       <Text onPress={pickImage}   style={styles.textBtn}>Select Image</Text>
       <Text style={styles.label}>Name</Text>
       <TextInput
